68616b4db041ff8034ed1f7cae3c2659ba66cf52
yoru@laptop <34027873+yuandahanif@users.noreply.github.com>
2024-06-04 16:53:29 +0700

    prototype mobile

---
+++ src/app/mobile/nav/home/page.tsx
@@ -0,0 +1,9 @@
+export default function MobileHome() {
+  return (
+    <main className="flex h-full w-full flex-col items-center justify-center p-5 gap-10">
+      <h1 className="text-2xl font-bold text-center">
+        Selamat datang di tempat sampah :3
+      </h1>
+    </main>
+  );
+}
--- src/app/mobile/page.tsx
+++ src/app/mobile/page.tsx
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function MobileLanding() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-24 gap-10">
+    <main className="flex h-full w-full flex-col items-center justify-center p-5 gap-10">
       <div className="relative w-96 h-96">
         <Image
           alt="trash can"
@@ -15,16 +15,15 @@ export default function Home() {
         />
       </div>
 
-      <h1 className="scroll-m-20 text-3xl font-bold tracking-tight">
+      <h1 className="text-2xl font-bold text-center">
         Selamat datang di tempat sampah :3
       </h1>
 
       <div className="flex gap-4 justify-center">
-        <Link href={"/mobile"}>
-          <Button>Download</Button>
-        </Link>
-        <Link href={"/dashboard"}>
-          <Button variant="ghost">Masuk</Button>
+        <Link href={"/mobile/nav/home"}>
+          <Button variant="default" className="bg-green-400 hover:bg-green-500">
+            Masuk
+          </Button>
         </Link>
       </div>
     </main>
