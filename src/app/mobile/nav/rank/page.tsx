a06a50787abc89e6c51fbc66908525b34dcd0f79
yuanda@yume <34027873+yuandahanif@users.noreply.github.com>
2024-06-05 17:53:06 +0700

    change profile rank

--- src/app/mobile/nav/rank/page.tsx
+++ src/app/mobile/nav/rank/page.tsx
@@ -6,15 +6,12 @@ import Image from "next/image";
 import { Permanent_Marker } from "next/font/google";
 import { CalendarSearch, ChevronDown, ChevronUp } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 const font = Permanent_Marker({
   weight: "400",
   subsets: ["latin"],
   variable: "--font-sans",
 });
-
-const tags = Array.from({ length: 47 }).map(
-  (_, i, a) => `${i + 4}. [nama siswa]`,
-);
 
 function is_fizz_buzz(n: number) {
   return n % 3 == 0 && n % 5 == 0;
@@ -26,6 +23,58 @@ function is_buzz(n: number) {
   return n % 5 == 0;
 }
 
+const names = [
+  "Jania Sherwood",
+  "Tucker Johnson",
+  "Stevie Daniel",
+  "Jair Gantt",
+  "Dan Brady",
+  "Bo Stevens",
+  "Cortez Aaron",
+  "Reina Delgado",
+  "Eduardo Hedges",
+  "Timmy Trimble",
+  "Elaina Hershberger",
+  "Dajuan Haggerty",
+  "Lillie Albert",
+  "Lamont Windham",
+  "Holland Dent",
+  "Misty Head",
+  "Darby Nugent",
+  "Ty Timmons",
+  "Gabriel Betancourt",
+  "Randi Almeida",
+  "Erin Sam",
+  "Khalid Orellana",
+  "Galilea Reinhart",
+  "Dara Cramer",
+  "Dorothy Hurd",
+  "Joslyn Staggs",
+  "Chris Robison",
+  "Hallie Musser",
+  "Demarcus McKinnon",
+  "Eleni Kessler",
+  "Bennett Laney",
+  "Dania Cao",
+  "Santiago Way",
+  "Amina Tackett",
+  "Jarret Oliveira",
+  "Olga Coombs",
+  "Kristi Boykin",
+  "Nash Toney",
+  "Kelly Ferreira",
+  "Rico Turner",
+  "Cloe Taylor",
+  "Kenton Grayson",
+  "Kory Fogle",
+  "Micheal Tyler",
+  "Jailene Ferguson",
+  "Racheal Villanueva",
+  "Justice Chamberlin",
+];
+
+const userRank = 3;
+
 export default function MobileRankPage() {
   return (
     <div className="relative flex h-full w-full flex-col p-1">
@@ -35,19 +84,18 @@ export default function MobileRankPage() {
             className={`relative mb-5 h-[120px] w-[120px] before:absolute before:-top-1 before:left-1/2 before:h-[200%] before:w-[60px] before:-translate-x-1/2 before:bg-[#c0c0c0] before:content-['']`}
           >
             <Image
-              src="https://safebooru.org//samples/4619/sample_5429d7b3d4bec2e681972180df2e673af1f39c65.jpg?4834439"
-              alt="https://i.pximg.net/img-original/img/2024/03/23/12/00/17/117170187_p1.png"
+              src="/images/p-1.jpg"
+              alt="https://www.freepik.com/free-vector/back-school-background-with-kids_2496999.htm#fromView=search&page=1&position=30&uuid=f9924c2d-db11-4ef2-a5ba-0f4818d9c09f"
               fill
-              className="clip-path-star -rotate-6 object-cover object-center shadow-md"
+              className="clip-path-star -rotate-6 object-cover object-center"
             />
           </div>
-
           <div
             className={`relative mb-10 h-[120px] w-[120px] before:absolute before:-top-1 before:left-1/2 before:h-[200%] before:w-[60px] before:-translate-x-1/2 before:bg-[#d4af37] before:content-['']`}
           >
             <Image
-              src="https://safebooru.org//images/4619/17289fb65697ec3adda05131d44dfdf150095533.png?4814699"
-              alt="https://i.pximg.net/img-original/img/2022/10/31/12/00/22/102382163_p11.png"
+              src="/images/p-2.jpg"
+              alt="https://www.freepik.com/free-vector/back-school-background-with-kids_2496999.htm#fromView=search&page=1&position=30&uuid=f9924c2d-db11-4ef2-a5ba-0f4818d9c09f"
               fill
               className="clip-path-star object-cover object-center"
             />
@@ -57,8 +105,8 @@ export default function MobileRankPage() {
             className={`relative mb-0 h-[120px] w-[120px] before:absolute before:-top-1 before:left-1/2 before:h-[200%] before:w-[60px] before:-translate-x-1/2 before:bg-[#cd7f32] before:content-['']`}
           >
             <Image
-              src="https://safebooru.org//images/4619/19969a20075ad17e6fa9084253d1400b43c64855.png?4814181"
-              alt="https://i.pximg.net/img-original/img/2022/10/31/12/00/22/102382163_p12.png"
+              src="/images/p-3.jpg"
+              alt="https://www.freepik.com/free-vector/back-school-background-with-kids_2496999.htm#fromView=search&page=1&position=30&uuid=f9924c2d-db11-4ef2-a5ba-0f4818d9c09f"
               fill
               className="clip-path-star rotate-6 object-cover object-center"
             />
@@ -85,12 +133,17 @@ export default function MobileRankPage() {
       </div>
 
       <ScrollArea className="h-1/2 w-full">
-        {tags.map((tag, idx) => (
+        {names.map((name, idx) => (
           <div
-            key={tag}
-            className="mb-1 flex justify-between rounded-lg border border-blue-300 bg-white p-3 text-sm shadow-md"
+            key={name}
+            className={cn(
+              "mb-1 flex justify-between rounded-lg border border-blue-300 bg-white p-3 text-sm shadow-md",
+              userRank == idx && "bg-blue-100",
+            )}
           >
-            <p>{tag}</p>{" "}
+            <p className="font-semibold">
+              {idx + 4} {name}
+            </p>
             {is_fizz_buzz(idx) ? (
               <ChevronUp className="text-green-500" />
             ) : is_buzz(idx) ? (
@@ -102,7 +155,6 @@ export default function MobileRankPage() {
             )}
           </div>
         ))}
-        <div className=""></div>
       </ScrollArea>
 
       <div className="flex w-full flex-grow rounded-lg rounded-t-none border border-blue-300 bg-white p-2">
